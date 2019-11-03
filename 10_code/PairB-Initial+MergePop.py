23730e7e6e8c881b51ae723afd2d906713ea2171
Jingyi Wu <jw612@duke.edu>
2019-11-03 12:10:13 -0500

    add code for merging population data

--- 10_code/PairB-Initial+MergePop.py
+++ 10_code/PairB-Initial+MergePop.py
@@ -4,7 +4,8 @@
 # This .py text file only makes the first step to initiate basic datasets for mortality analysis
 # Only includes basic subsetting and reshaping
 
-# The code below mainly initiates 2 basic mortality dataframes on county and state level respectively
+# The code below mainly initiates two basic mortality dataframes on county and state level respectively
+# Specifically, the Initial Dataset is the dataframe - 'mortality'
 # The next step would be merge with population dataset to do normalization
 
 
@@ -57,26 +58,109 @@ mortality_all
 ######## County Level Mortality Dataset ########
 
 # Sum up deaths caused by all types of drug overdose at county level
-mortality_county = mortality_all.groupby(['Year','State','County']).sum().reset_index()
-mortality_county
+mortality = mortality_all.groupby(['Year','State','County']).sum().reset_index()
+mortality.columns = ['Year', 'State_Code', 'County', 'Deaths']
+mortality
 
 # Check if our groupby is correct
 # i.e. Check duplications based on multiple columns on 'Year', 'State' AND 'County'
-assert not mortality_county.duplicated(['Year','State','County']).any()
+assert not mortality.duplicated(['Year','State_Code','County']).any()
 
 
 ######## State Level Mortality Dataset ########
 # Sum up deaths caused by all types of drug overdose at state level
-mortality_state = mortality_all.groupby(['Year','State']).sum().reset_index()
-mortality_state
+# mortality_state = mortality.groupby(['Year','State']).sum().reset_index()
+# mortality_state
 
 # Check if our groupby is correct
 # i.e. Check duplications based on multiple columns on 'Year' AND 'State'
-assert not mortality_state.duplicated(['Year','State']).any()
+# assert not mortality_state.duplicated(['Year','State']).any()
 
 
-######## Country Level Mortality Dataset (only for reference) ########
-# Sum up deaths caused by all types of drug overdose at Country(USA) level
-# aka time trends in whole USA
-mortality_USA = mortality_all.groupby('Year').sum().reset_index()
-mortality_USA
+
+######## README ########
+
+# Mainly contains population data cleaning and merging with Initial Dataset
+# Specifically, the Merged Dataset is the dataframe - 'merge_mortality'
+# The next step would be examining statistics and sample selection
+
+# Import source data of population
+pop = pd.read_excel('~/estimating-impact-of-opioid-prescription-regulations-team-2/00_source/Population2010_AllCounties.xls')
+pop.head(10)
+pop.tail(10)
+
+# Subsetting to get necessary information
+pop = pop.iloc[6:3229,0:4].copy().reset_index(drop=True)
+
+# Rename columns for later merging check
+pop.columns = ['County', 'State_Name', 'Population_2000', 'Population_2010']
+pop = pop.dropna(how='all')
+pop.tail()
+
+# Check the dataframe after deleting NaN values
+assert not pop['County'].isnull().any()
+
+# Since we only have state name in population data
+# and only state code in the initial dataset
+# find a dictionary as mapping
+
+# Import states name dictionary data
+state_dic = pd.read_csv('~/estimating-impact-of-opioid-prescription-regulations-team-2/00_source/states_name_dic.csv')
+# Rename columns for later merging check
+state_dic.columns = ['State_Name','State_Code', 'FIPS']
+state_dic.head()
+
+# Merging county_level mortality data with dictionary data
+merge_dic = pd.merge(mortality, state_dic, how='left', on='State_Code', indicator=True)
+
+# Check if our merge is correct
+both_check = merge_dic['_merge']=='both'
+assert both_check.all()
+
+# Extract necessary columns
+# Could delete one state code column since '_merge' are all 'both'
+merge_dic = merge_dic[['Year', 'State_Code', 'State_Name', 'County', 'Deaths']]
+merge_dic.tail()
+
+# Then merge with population data
+merge_mortality = pd.merge(merge_dic, pop, how='left', on=['State_Name','County'], indicator=True)
+merge_mortality.head()
+
+# Check unmatched information in merging
+merge_mortality['_merge'].value_counts()
+
+# Check if there is missed information in DC, FL and TX
+miss = merge_mortality[merge_mortality['_merge'] != 'both']
+miss[(miss['State_Code'] == 'DC')|(miss['State_Code'] == 'FL')|(miss['State_Code'] == 'TX')]
+
+# Check if there is different naming for a state
+import re
+for i in state_dic['State_Name'].unique():
+    if re.match(".*D.*",i):
+        print(i)
+
+# Go back to original dictionary data to verify
+state_dic[(state_dic['State_Name']=='District of Columbia')|(state_dic['State_Name']=='D.C.')]
+
+# Unify the naming for District of Columbia
+state_dic['State_Name'][state_dic['State_Name'] == 'D.C.'] = 'District of Columbia'
+
+# Redo merge
+merge_dic = pd.merge(mortality, state_dic, how='left', on='State_Code')
+merge_dic = merge_dic[['Year', 'State_Code', 'State_Name', 'County', 'Deaths']]
+merge_mortality = pd.merge(merge_dic, pop, how='left', on=['State_Name','County'], indicator=True)
+
+# Check unmatched information
+merge_mortality['_merge'].value_counts()
+
+# There are still some unmatched
+# Deal with them later if needed when come to sample selection
+miss = merge_mortality[merge_mortality['_merge'] != 'both']
+miss['State_Name'].value_counts()
+
+# Drop state AK
+merge_mortality = merge_mortality[merge_mortality['State_Code']!='AK'].reset_index(drop=True)
+
+# Check if there is NaN in population for DC, FL and TX
+# We got our merged dataset ready for the next step
+assert not merge_mortality[(merge_mortality['State_Code']=='DC')|(merge_mortality['State_Code']=='FL')|(merge_mortality['State_Code']=='TX')]['Population_2010'].isnull().any()
