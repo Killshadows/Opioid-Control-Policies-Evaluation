e7d36a8dce3c147ff349680313eb43dbb6190ce5
Jingyi Wu <jw612@duke.edu>
2019-11-17 21:53:39 -0500

    add code for heat map

--- 10_code/PairB-03-StatsOverView.py
+++ 10_code/PairB-03-StatsOverView.py
@@ -1,16 +1,21 @@
-######### README ########
+######## README ########
 
-# Stats Overview for the mortality data
-# Input df : mortality_pop_norm
+# For easier bugs checking and fixing
+# This .py text file only makes the first step to initiate basic datasets for mortality analysis
+# Only includes basic subsetting and reshaping
 
-## Creating and loading the merged data from the intermidiate file.
+# The code below mainly initiates two basic mortality dataframes on county and state level respectively
+# Specifically, the Initial Dataset is the dataframe - 'mortality'
+# The next step would be merge with population dataset to do normalization
+
 
 import pandas as pd
 import numpy as np
 
 # Import data from source dataset
-mortality_int = pd.read_csv('../20_intermediate_files/pairB_intermidiate.csv')
+mortality_int = pd.read_csv('~/estimating-impact-of-opioid-prescription-regulations-team-2/20_intermediate_files/pairB_intermidiate.csv')
 mortality_int
+
 
 ######## Learn more about the Initial Dateset ########
 
@@ -80,7 +85,7 @@ assert not mortality.duplicated(['Year','State_Code','County']).any()
 # The next step would be examining statistics and sample selection
 
 # Import source data of population
-pop = pd.read_excel('../00_source/Population2010_AllCounties.xls')
+pop = pd.read_excel('~/estimating-impact-of-opioid-prescription-regulations-team-2/00_source/Population2010_AllCounties.xls')
 pop.head(10)
 pop.tail(10)
 
@@ -100,7 +105,7 @@ assert not pop['County'].isnull().any()
 # find a dictionary as mapping
 
 # Import states name dictionary data
-state_dic = pd.read_csv('../00_source/states_name_dic.csv')
+state_dic = pd.read_csv('~/estimating-impact-of-opioid-prescription-regulations-team-2/00_source/states_name_dic.csv')
 # Rename columns for later merging check
 state_dic.columns = ['State_Name','State_Code', 'FIPS']
 state_dic.head()
@@ -124,11 +129,18 @@ merge_mortality.head()
 # Check unmatched information in merging
 merge_mortality['_merge'].value_counts()
 
-# Check if there is missed information in DC, FL and TX
+# Check if there is missed information
+# Seems no missing information in WA, FL and TX
+# But do have missing in some other states
 miss = merge_mortality[merge_mortality['_merge'] != 'both']
-miss[(miss['State_Code'] == 'DC')|(miss['State_Code'] == 'FL')|(miss['State_Code'] == 'TX')]
+miss
+miss['_merge'].value_counts()
+miss[(miss['State_Code'] == 'WA')|(miss['State_Code'] == 'FL')|(miss['State_Code'] == 'TX')]
 
-# Check if there is different naming for a state
+# Check which state has missing information
+miss['State_Name'].value_counts()
+
+# Check if there is different naming for DC
 import re
 for i in state_dic['State_Name'].unique():
     if re.match(".*D.*",i):
@@ -140,25 +152,87 @@ state_dic[(state_dic['State_Name']=='District of Columbia')|(state_dic['State_Na
 # Unify the naming for District of Columbia
 state_dic['State_Name'][state_dic['State_Name'] == 'D.C.'] = 'District of Columbia'
 
-# Redo merge
+# Check if there is different naming for Rhode Island and Providence Plantations
+for i in state_dic['State_Name'].unique():
+    if re.match(".*hode.*",i):
+        print(i)
+
+# Go back to original dictionary data to verify
+state_dic[(state_dic['State_Name']=='Rhode Island')|(state_dic['State_Name']=='Rhode Island and Providence Plantations')]
+
+# Unify the naming for Rhode Island and Providence Plantations
+state_dic['State_Name'][state_dic['State_Name'] == 'Rhode Island and Providence Plantations'] = 'Rhode Island'
+
+# Check if there is different naming for New Mexico, Indiana and Pennsylvania
+# seems they are not from the problem of different naming
+for i in state_dic['State_Name'].unique():
+    if re.match(".*exi.*",i):
+        print(i)
+for i in state_dic['State_Name'].unique():
+    if re.match(".*ndia.*",i):
+        print(i)
+for i in state_dic['State_Name'].unique():
+    if re.match(".*enns.*",i):
+        print(i)
+
+
+# Redo merge first
 merge_dic = pd.merge(mortality, state_dic, how='left', on='State_Code')
 merge_dic = merge_dic[['Year', 'State_Code', 'State_Name', 'County', 'Deaths']]
 merge_mortality = pd.merge(merge_dic, pop, how='left', on=['State_Name','County'], indicator=True)
 
-# Check unmatched information
+# Check still missing information
 merge_mortality['_merge'].value_counts()
-
-# There are still some unmatched
-# Deal with them later if needed when come to sample selection
 miss = merge_mortality[merge_mortality['_merge'] != 'both']
-miss['State_Name'].value_counts()
+miss
 
-# Drop state AK
-merge_mortality = merge_mortality[merge_mortality['State_Code']!='AK'].reset_index(drop=True)
+# Check if there is something wrong with Mc Kean County in pop data
+for i in pop['County'].unique():
+    if re.match(".*ean.*",i):
+        print(i)
+for i in mortality['County'].unique():
+    if re.match(".*ean.*",i):
+        print(i)
+# Unify the naming for Mc Kean County
+pop['County'][pop['County']=='McKean County'] = 'Mc Kean County'
 
-# Check if there is NaN in population for DC, FL and TX
+# Check if there is something wrong with Dona Ana County in pop data
+for i in pop['County'].unique():
+    if re.match(".*Ana.*",i):
+        print(i)
+for i in mortality['County'].unique():
+    if re.match(".*Ana.*",i):
+        print(i)
+# Unify the naming for Dona Ana County
+pop['County'][pop['County']=='Doña Ana County'] = 'Dona Ana County'
+
+# Check if there is something wrong with La Porte County in pop data
+for i in pop['County'].unique():
+    if re.match(".*orte.*",i):
+        print(i)
+for i in mortality['County'].unique():
+    if re.match(".*orte.*",i):
+        print(i)
+# Unify the naming for La Porte County
+pop['County'][pop['County']=='LaPorte County'] = 'La Porte County'
+
+# Redo merge again
+merge_dic = pd.merge(mortality, state_dic, how='left', on='State_Code')
+merge_dic = merge_dic[['Year', 'State_Code', 'State_Name', 'County', 'Deaths']]
+merge_mortality = pd.merge(merge_dic, pop, how='left', on=['State_Name','County'], indicator=True)
+
+# Check still missing information
+# Ignore Alaska and will drop it later
+merge_mortality['_merge'].value_counts()
+miss = merge_mortality[merge_mortality['_merge'] != 'both']
+miss
+
+# Reset index of merged data
+merge_mortality = merge_mortality.reset_index(drop=True)
+
+# Check if there is NaN in population for WA, FL and TX
 # We got our merged dataset ready for the next step
-assert not merge_mortality[(merge_mortality['State_Code']=='DC')|(merge_mortality['State_Code']=='FL')|(merge_mortality['State_Code']=='TX')]['Population_2010'].isnull().any()
+assert not merge_mortality[(merge_mortality['State_Code']=='WA')|(merge_mortality['State_Code']=='FL')|(merge_mortality['State_Code']=='TX')]['Population_2010'].isnull().any()
 
 # Normalization by 2010 Population
 merge_mortality['Deaths_NormPop2010'] = merge_mortality['Deaths']/ merge_mortality['Population_2010']
@@ -168,7 +242,7 @@ merge_mortality.head()
 # Rename the columns the same as required in action plan
 mortality_pop_norm = merge_mortality[['Year','State_Code','County','Deaths','Population_2010','Deaths_NormPop2010']]
 mortality_pop_norm.columns = ['Year','State','County','Deaths','Population','Deaths_PerCap_County']
-mortality_pop_norm.head()
+mortality_pop_norm
 
 
 ### Stats overviw
@@ -177,7 +251,7 @@ mortality_pop_norm.head()
 from plotnine import *
 mortality_pop_norm.loc[:,"County"].unique().size
 
-#What is the distribution of deaths on county / state level? 
+#What is the distribution of deaths on county / state level?
 #A heat map for deaths / population, if possible….
 pt_c = pd.pivot_table(mortality_pop_norm, index = "County", columns = "Year", values = "Deaths")
 pt_c.head()
@@ -201,6 +275,7 @@ pt_c[pt_c.mean_c == min(pt_c.mean_c)].index.size
 
 
 ## State-Level
+mortality_pop_norm['Deaths_PerCap_County'] = mortality_pop_norm['Deaths_PerCap_County'].astype('float')
 pt_s = mortality_pop_norm.groupby(['State']).mean()
 pt_s.head()
 
@@ -213,19 +288,39 @@ d = mortality_pop_norm.groupby(["State", "Year"]).mean()
 d.reset_index(inplace=True)
 
 ## Plot for each state
-(ggplot(d, aes(x = "Year", y = "Deaths_PerCap_County", color = "State")) 
+(ggplot(d, aes(x = "Year", y = "Deaths_PerCap_County", color = "State"))
 + geom_line()
-+ labs(x = "Year", y = "Deaths rate", title = "States Deaths Trend"))
++ labs(x = "Year", y = "Drug Deaths Per Cap", title = "States Deaths Trend"))
 
 
 ## Plot for national trend
 national = d.groupby(["Year"]).mean()
 national.reset_index(inplace = True)
 
-(ggplot(national, aes(x = "Year", y = "Deaths_PerCap_County")) 
+(ggplot(national, aes(x = "Year", y = "Deaths_PerCap_County"))
  + geom_line()
  + labs(x = "Year", y = "Deaths rate", title = "National Deaths Trend"))
 
-## TODO: Heat map
+## Heat map
+# Get Deaths per ten thousand people on state level
+mortality_pop_norm.head()
+mortality_state = mortality_pop_norm.groupby(["State"]).mean().reset_index()
+mortality_state['Deaths_PerTenThousand_State'] = mortality_state['Deaths_PerCap_County']*10000
+mortality_state
 
+import plotly.graph_objects as go
 
+fig = go.Figure(data=go.Choropleth(
+    locations=mortality_state['State'], # Spatial coordinates
+    z = mortality_state['Deaths_PerTenThousand_State'], # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Blues',
+    colorbar_title = "Drug Deaths Per Cap",
+    marker_line_color='white',
+    marker_line_width=0.5
+))
+fig.update_layout(
+    title_text = 'US Drug Deaths Per Ten Thousand People by State, 2004-2015)',
+    geo_scope='usa', # limit map scope to USA
+)
+fig.show()
